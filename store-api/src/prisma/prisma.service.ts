98beb4fdcea4e29c86aa666c95ca7db549cab59d
islam <abdelrahmani@aecl.com>
2026-08-11 00:48:57 +0300

    feat: add Prisma with User model and wire PrismaModule into NestJS

---
+++ store-api/src/prisma/prisma.service.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '../../generated/prisma/client';
+import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor(config: ConfigService) {
+    super({
+      adapter: new PrismaPg({
+        connectionString: config.getOrThrow<string>('DATABASE_URL'),
+      }),
+    });
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect();
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect();
+  }
+}
